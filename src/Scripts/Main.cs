535a838e351e497b4f3963b24ab9d06bdfb3f359
Airbus5717 <abdussamadf350@gmail.com>
2020-08-31 09:54:21 +0300

    add Main Files
    #1 first commit

---
+++ src/Scripts/Main.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public class Main : Control
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	//  public override void _Process(float delta)
+	//  {
+	//      
+	//  }
+
+	/// OPEN FILE
+
+	private void openFileButtonPressed()
+	{
+		GetNode<FileDialog>("openFileDialog").Popup_();
+	}
+
+
+	private void openFileDialogFileSelected(String path)
+	{
+		GD.Print(path);
+		var f = new File();
+		
+		f.Open(path , File.ModeFlags.Read);
+		GetNode<TextEdit>("TextEditor").Text = f.GetAsText();
+
+	}
+
+	/// SAVE AS FILE
+	
+	private void saveAsFileButtonPressed()
+	{
+		GetNode<FileDialog>("saveAsFileDialog").Popup_();
+	}
+	private void saveAsFileDialogFileSelected(String path)
+	{
+		var f = new File();
+		f.Open(path, File.ModeFlags.Write);
+		f.StoreString(GetNode<TextEdit>("TextEditor").Text);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
