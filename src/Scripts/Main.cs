2fe7d7d1e07080ff46b53adf9b5e28c0804f65bc
Airbus5717 <abdussamadf350@gmail.com>
2020-09-02 14:09:20 +0300

    #5

--- src/Scripts/Main.cs
+++ src/Scripts/Main.cs
@@ -3,24 +3,27 @@ using System;
 
 public class Main : Control
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-	// Called when the node enters the scene tree for the first time.
+    // private int a = 2;
+    // private string b = "text";
+    // Called when the node enters the scene tree for the first time.
+    
+	/*
 	public override void _Ready()
 	{
 		
 	}
+	
 
 	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
 	//  public override void _Process(float delta)
 	//  {
 	//      
 	//  }
+	*/
 
-	/// OPEN FILE
+    /// OPEN FILE
 
-	private void openFileButtonPressed()
+    private void openFileButtonPressed()
 	{
 		GetNode<FileDialog>("openFileDialog").Popup_();
 	}
@@ -51,15 +54,3 @@ public class Main : Control
 		f.Close();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
